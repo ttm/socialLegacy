41b08d1f4944842859d37e65c091db5f16158ee1
Renato Fabbri <renato.fabbri@gmail.com>
2015-05-20 12:13:35 -0300

    feita paralelização da busca de twitter

--- tests/testTwitter2.py
+++ tests/testTwitter2.py
@@ -1,6 +1,7 @@
-import twython as T, pickle, maccess
+import twython as T, maccess, time
+import multiprocessing as mp
 
-terms=["#python",
+terms=["#jesus",
        "#g1",
        "#prayforthephilippines",
        "#felipao",
@@ -9,6 +10,7 @@ terms=["#python",
        "#dilma",
        "#science",
        "#god"]
+fnames=["pickleDir/{}".format(term.replace("#","HASH")) for term in terms]
 twitters=[]
 searchs=[]
 for tw,term in zip(maccess.TW,terms):
@@ -16,8 +18,41 @@ for tw,term in zip(maccess.TW,terms):
                       app_secret=         tw.taks,
                       oauth_token=        tw.tat ,
                       oauth_token_secret= tw.tats)
-    search = twitter.search_gen(term)
+    search= twitter.cursor(twitter.search, q=term)
+    #search = twitter.search_gen(term)
     twitters.append(twitter)
     searchs.append(search)
 
+def rodaSearch(search,fname,output,rr=None):
+    if not rr:
+        rr=[]
+    try:
+        for result in search:
+            print("result "+fname)
+            rr.append(result)
+    except:
+        #f=open(fname,"wb")
+        #rrr=rr[:]
+        #pickle.dump(rrr,f,-1)
+        #f.close()
+        time.sleep(15*60)
+        rodaSearch(search,fname,output,rr)
+    #f=open(fname,"wb")
+    #rrr=rr[:]
+    #pickle.dump(rrr,f,-1)
+    #f.close()
+    output.put(rr)
 
+#pool=mp.Pool(processes=9)
+#results__=[pool.apply_async(rodaSearch,args=(search,fname)) for search,fname in zip(searchs,fnames)]
+output=mp.Queue()
+processes=[mp.Process(target=rodaSearch,args=(search,fname,output)) for search,fname in zip(searchs,fnames)]
+
+
+#output=[p.get() for p in results__]
+for p in processes:
+    p.start()
+for p in processes:
+    p.join()
+results=[output.get() for p in processes]
+print(results)
