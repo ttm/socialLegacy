eaad1d6fea24a9b1df5f8838c124a5726914421d
Renato Fabbri <renato.fabbri@gmail.com>
2015-12-02 13:33:33 -0200

    feitas gambis para streamar os tweets

--- tests/rdfTwitter.py
+++ tests/rdfTwitter.py
@@ -7,8 +7,10 @@ c=P.utils.check
 
 scriptpath=os.path.realpath(__file__)
 fpath="./publishing/tw/"
-fnames=["python.pickle",]
-b=S.tw.publishSearch("../data/tw/{}".format(fnames[0]),fpath,
-        None,scriptpath,datetime.date(2015, 10, 15),
-        "tweets with the #python hashtag")
+fnames=["python.pickle","arenaNETmundial.pickle"]
+descriptions=["tweets with the #python hashtag","tweets with the #arenaNETmundial hashtag"]
+for fname,desc in zip(fnames,descriptions):
+    b=S.tw.publishSearch("../data/tw/{}".format(fname),fpath,
+            None,scriptpath,datetime.date(2015, 10, 15),
+            desc)
 #
---
+++ tests/simpleTwitterStream.py
@@ -0,0 +1,38 @@
+import social as S, percolation as P, os, re
+import  importlib, datetime
+importlib.reload(S.tw)
+importlib.reload(P.utils)
+import maccess
+c=P.utils.check
+ttag,tid="#MAMA2015",2
+ttag,tid="#SyriaVote",1
+ttag,tid="#art",6
+ttag,tid="#god",7
+ttag,tid="#porn",4 #fuck pouco pouquissimo fluxo, tentar #porn
+ttag,tid="#ChennaiFloods",3
+ttag,tid="#QuartaSemRacismoClubeSDV",0
+ttag,tid="#evil",8 #shit pouquissimo fluxo, tentar #devil ou #evil
+
+tws=S.tw.Twitter(*P.utils.cred(maccess.TW[tid]))
+#tws.streamTag(ttag)
+# dont forguet to tws.finishStream() afterwards
+# load with P.utils.pRead2("../data/tw/{}_tw_.pickle".format(ttag[1:]))
+# will hava a list of 100 tweets per element
+try:
+    tws.streamTag(ttag)
+except:
+    c("erro")
+    try:
+        tws.streamTag(ttag)
+    except:
+        c("erro")
+        try:
+            tws.streamTag(ttag)
+        except:
+            c("erro")
+            try:
+                tws.streamTag(ttag)
+            except:
+                c("erro")
+                tws.streamTag(ttag) # erros ao todo pode dar de decodificacao
+
